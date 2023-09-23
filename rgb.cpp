1d0d385c5534b6ceb2d651a79b66197dce399c61
Imran Remtulla <imranr98@pm.me>
2023-09-22 20:28:54 -0400

    RGB tweaks

--- rgb.cpp
+++ rgb.cpp
@@ -6,59 +6,54 @@ void setRGB(int redValue, int greenValue, int blueValue) {
   analogWrite(RGB_LED_BLUE, 255 - blueValue);
 }
 
-void turnOffRGB() { setRGB(0, 0, 0); }
+void turnOffRGB() { setColor(0, 0, 0); }
 
-void fadeInRGB(int redStart, int greenStart, int blueStart, int redEnd,
-               int greenEnd, int blueEnd, int loopDelay) {
+void fadeInRGB(int red, int green, int blue, int redEnd, int greenEnd,
+               int blueEnd, int loopDelay) {
   bool complete = false;
-  int red = redStart < redEnd ? redStart : redEnd;
-  int green = greenStart < greenEnd ? greenStart : greenEnd;
-  int blue = blueStart < blueEnd ? blueStart : blueEnd;
-  int redComplete = redStart < redEnd ? redEnd : redStart;
-  int greenComplete = greenStart < greenEnd ? greenEnd : greenStart;
-  int blueComplete = blueStart < blueEnd ? blueEnd : blueStart;
+  bool redRev = redEnd < red;
+  bool greenRev = greenEnd < green;
+  bool blueRev = blueEnd < blue;
 
   while (!complete) {
     complete = true;
-    if (red != redComplete) {
-      red++;
+    if (red != redEnd) {
+      redRev ? red-- : red++;
       complete = false;
     }
-    if (green != greenComplete) {
-      green++;
+    if (green != greenEnd) {
+      greenRev ? green-- : green++;
       complete = false;
     }
-    if (blue != blueComplete) {
-      blue++;
+    if (blue != blueEnd) {
+      blueRev ? blue-- : blue++;
       complete = false;
     }
-    setRGB(red, green, blue);
+    setColor(red, green, blue);
     delay(loopDelay);
   }
 }
 
-void blinkRGBCode(String str) {
-  fadeInRGB(0, 0, 0, 255, 255, 255, 15);
-  turnOffRGB();
+void blinkRGB(String str) {
+  fadeInRGB(0, 0, 0, 0, 0, 255, 10);
+  setColor(0, 255, 255);
+  delay(30);
+  turnOffLED();
   delay(500);
-
   for (int i = 0; i < str.length(); i++) {
     char digit = str.charAt(i);
     int blinkCount = digit - '0'; // Convert the character to an integer
     long red = i % 2 == 0 ? 0 : 255;
     long green = i % 2 == 0 ? 255 : 0;
     long blue = 0;
-
     Serial.println("Digit: " + String(digit) +
                    " Blinks: " + String(blinkCount) + " RGB: " + String(red) +
                    " " + String(green) + " " + String(blue));
-
-    // Blink the RGB LED to represent the current digit
-    delay(1000);
+    delay(1500);
     for (int j = 0; j < blinkCount; j++) {
-      setRGB(red, green, blue);
+      fadeInRGB(0, 0, 0, red, green, blue, 2);
       delay(700);
-      turnOffRGB();
+      fadeInRGB(red, green, blue, 0, 0, 0, 2);
       delay(300);
     }
   }
